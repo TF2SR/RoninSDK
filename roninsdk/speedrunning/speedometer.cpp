68e7196ebc4faafefe16b2dfb324871ba9626dab
EladNLG <e1lad8955@gmail.com>
2024-09-13 09:25:35 +0300

    rename "thing"

--- roninsdk/speedrunning/speedometer.cpp
+++ roninsdk/speedrunning/speedometer.cpp
@@ -5,7 +5,7 @@
 SQRESULT Script_Ronin_GetPlayerPlatformVelocity(HSquirrelVM* sqvm)
 {
 	CMemory player = CMemory(g_pSQManager<ScriptContext::CLIENT>->GetEntity<void>(sqvm, 1));
-	uintptr_t thing = player.Offset(0x380).GetPtr();
-	g_pSQManager<ScriptContext::CLIENT>->PushVector(sqvm, (const SQFloat*)thing);
+	uintptr_t platVelocityVector = player.Offset(0x380).GetPtr();
+	g_pSQManager<ScriptContext::CLIENT>->PushVector(sqvm, (const SQFloat*)platVelocityVector);
 	return SQRESULT_NOTNULL;
 }
